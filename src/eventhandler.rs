30859958c085e01d0fc2074e98d145982519ef38
ProgramFOX <programfox@hotmail.be>
2018-08-23 19:36:19 +0200

    Perform moderator action in different thread with .wait

--- src/eventhandler.rs
+++ src/eventhandler.rs
@@ -5,16 +5,12 @@ use hyper::{Body, Client, Request};
 use hyper_tls::HttpsConnector;
 use signup::rules::*;
 use std::sync::mpsc::Receiver;
+use std::thread;
 
 pub fn handle_events(rx: Receiver<Event>, token: &'static str, rules_path: &'static str) {
     let rule_manager =
         SignupRulesManager::new(rules_path.to_string()).expect("could not load rules");
     println!("Currently {} rules.", rule_manager.rules.len());
-
-    let https = HttpsConnector::new(4).unwrap();
-    let client = Client::builder().build::<_, Body>(https);
-
-    let bearer = "Bearer ".to_owned() + token;
 
     loop {
         let event = rx.recv().unwrap();
@@ -36,6 +32,10 @@ pub fn handle_events(rx: Receiver<Event>, token: &'static str, rules_path: &'sta
                         &user_agent,
                         &finger_print,
                     )) {
+                        let https = HttpsConnector::new(1).unwrap();
+                        let client = Client::builder().build::<_, Body>(https);
+
+                        let bearer = "Bearer ".to_owned() + token;
                         let mut action_req = Request::new(Body::from(""));
                         *action_req.uri_mut() =
                             rule.action.api_endpoint(&username).parse().unwrap();
@@ -43,7 +43,19 @@ pub fn handle_events(rx: Receiver<Event>, token: &'static str, rules_path: &'sta
                             hyper::header::AUTHORIZATION,
                             HeaderValue::from_str(&bearer).unwrap(),
                         );
-                        // TODO: perform action_req
+
+                        thread::spawn(move || {
+                            match client
+                                .request(action_req)
+                                .map(|_| println!("Action succesful."))
+                                .map_err(|err| {
+                                    println!("Error on mod action: {}", err);
+                                }).wait()
+                            {
+                                Err(_) => println!("Error on mod action .wait()"),
+                                _ => {}
+                            };
+                        });
                     }
                 }
             }

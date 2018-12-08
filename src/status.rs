f1fa43ece062694c2bca4791bc1219e8c98b5a4a
ProgramFOX <programfox@hotmail.be>
2018-12-08 18:30:05 +0100

    Remove excessive logging

--- src/status.rs
+++ src/status.rs
@@ -32,7 +32,6 @@ pub fn status_loop(
                     Ok(Loop::Continue((Instant::now(), latest_slack_event)))
                 }
                 StatusPing::EnsureAliveConnectionLichess => {
-                    println!("EnsureAliveConnectionLichess received");
                     if latest_stream_event.elapsed().as_secs() > 90 {
                         eventstream::watch_event_stream(main_tx.clone(), token, status_tx.clone());
                         println!("Event stream watcher restarted.");
@@ -45,7 +44,6 @@ pub fn status_loop(
                     Ok(Loop::Continue((latest_stream_event, Instant::now())))
                 }
                 StatusPing::EnsureAliveConnectionSlack => {
-                    println!("EnsureAliveConnectionSlack received");
                     if latest_slack_event.elapsed().as_secs() > 720 {
                         slack::rtm::connect_to_slack(
                             conf::SLACK_BOT_TOKEN,
@@ -66,13 +64,10 @@ pub fn status_loop(
 }
 
 pub fn periodically_ensure_alive_connection(status_tx: Sender<StatusPing>) {
-    println!("in periodically_...");
     tokio::spawn(loop_fn((), move |_| {
-        println!("New iteration of perodically_ensure_alive_connection");
         let status_tx2 = status_tx.clone();
         Delay::new(Instant::now() + Duration::from_secs(15))
             .and_then(move |_| {
-                println!("StatusPing::* sending");
                 status_tx2
                     .send(StatusPing::EnsureAliveConnectionLichess)
                     .unwrap();
